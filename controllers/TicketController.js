9438da3013ccb85fea51cf99224b0ebbf91cec70
chanjbc <38666443+chanjbc@users.noreply.github.com>
2023-03-25 12:37:55 -0400

    ticket controller fixed

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -3,7 +3,7 @@ const { ticket } = require("../models");
 
 exports.viewTickets = async(req, res) => {
     const { flightId } = req.body;
-    const targetFlight = await flight.findOne({ where: { flightId: flightId }});
+    const targetFlight = await flight.findOne({ where: { id: flightId }});
     if (!targetFlight) return res.status(400).json({ error: "Flight doesn't exist!" });
     return res.status(200).json(await ticket.findAll({ where : { flightId: flightId }}));
 };
