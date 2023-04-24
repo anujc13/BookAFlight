598d9e799ac324c7b7de1a22332ff2fabae8e485
chanjbc <chan.jbc@gmail.com>
2023-04-24 02:19:26 -0400

    working on auto-population

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express");
 const path = require("path");
 const db = require("./models");
+const axios = require("axios");
+const express = require("express");
+const Amadeus = require("amadeus");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,31 +17,118 @@ const planeModelRoute = require("./routes/PlaneModel");
 const seatPositionRoute = require("./routes/SeatPosition");
 const ticketRoute = require("./routes/Ticket");
 
+
+// establish routes, with default going to homepage and none going to invalidpage
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"));
 });
-
 app.use("/booking", bookingRoute);
 app.use("/customer", customerRoute);
 app.use("/flight", flightRoute);
 app.use("/planeModel", planeModelRoute);
 app.use("/seatPosition", seatPositionRoute);
 app.use("/ticket", ticketRoute);
-
-// page not found
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, "/views/invalidpage.html"));
 });
 
-db.sequelize.sync().then(() => {
+const toIsoString = date => {
+    let tzo = -date.getTimezoneOffset(),
+        pad = num => {
+            return (num < 10 ? '0' : '') + num;
+        };
+  
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes());
+}
+const amadeus = new Amadeus({
+    clientId: "GpKk6tHJc33cfhxvPLktp1viSwpJa56B",
+    clientSecret: "pAPfloCr1XaA0toB"
+});
+
+
+db.sequelize.sync().then(async () => {
+
+    // AMADEUS
+
+    // FLIGHTSTATS
+    /*
+    const options = {
+        method: "GET",
+        url: "https://api.flightstats.com/flex/schedules/rest/v1/json/from/JFK/departing/2023/4/28/13",
+        headers: {
+            appId: "fd6db579",
+            appKey: "3b61de257c2e01cb9c3d3442ac673361"
+        }
+    };
+    try {
+        const response = await axios.request(options);
+        const { scheduledFlights } = response.data;
+        scheduledFlights.forEach(element => {
+            console.log(element);
+        })
+    } catch (err) {
+        console.log(err);
+    }
+    */
+
+    // AERODATABOX
+    /*
+    // query aerodatabox for flights flying to/from JFK over the next 11 hours
+    let dStart = new Date();
+    let dEnd = new Date();
+    dEnd.setHours(dStart.getHours() + 11);
+    dStart = toIsoString(dStart);
+    dEnd = toIsoString(dEnd);
+    const options = {
+        method: 'GET',
+        url: 'https://aerodatabox.p.rapidapi.com/flights/airports/iata/JFK/' + dStart + '/' + dEnd,
+        params: {
+            withCancelled: 'false',
+            withCodeshared: 'true',
+            withCargo: 'false',
+            withPrivate: 'false',
+            withLocation: 'false'
+        },
+        headers: {
+            'content-type': 'application/octet-stream',
+            'X-RapidAPI-Key': 'b6bd7f3f44mshc9228f8f9eb4c25p138c47jsn9181c49ffc94',
+            'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com'
+        }
+    };
+    try {
+        const flights = await axios.request(options);
+        const { departures, arrivals } = flights.data;
+
+    } catch (err) {
+        console.error(err);
+    }
+    */
+
+    // SKELETON FOR CREATING FLIGHTS
+    /*
+    await db.flight.create({
+        sourceAirport: ,
+        destinationAirport: ,
+        departureTime: ,
+        arrivalTime: ,
+        duration: ,
+        airline: ,
+        basePrice: ,
+        numSold: ,
+    })
+    */
+
+    
     app.listen(PORT, () => {
         console.log(`Server listening at http://localhost:${PORT}`);
     });
 
-
-
     /*
-    // seeding database
+    // manually seeding database
     db.planeModel.count().then(async numPlaneModels => {
         if (numPlaneModels === 0) {
             // create Boeing 737 plane model
